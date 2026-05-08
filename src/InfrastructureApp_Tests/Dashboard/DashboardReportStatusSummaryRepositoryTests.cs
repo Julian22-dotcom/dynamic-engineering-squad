721ab2fd217db890283858ea008cc7ae209e5c52
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2026-05-07 21:24:38 -0700

    Added dashboard status summary test setup/helpers

--- src/InfrastructureApp_Tests/Dashboard/DashboardReportStatusSummaryRepositoryTests.cs
+++ src/InfrastructureApp_Tests/Dashboard/DashboardReportStatusSummaryRepositoryTests.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using InfrastructureApp.Data;
+using InfrastructureApp.Models;
+using InfrastructureApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 
 namespace InfrastructureApp_Tests.Dashboard
@@ -6,5 +17,86 @@ namespace InfrastructureApp_Tests.Dashboard
     [TestFixture]
     public class DashboardReportStatusSummaryRepositoryTests
     {
+        private ApplicationDbContext _db = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("DashboardReportStatusSummaryRepositoryTest_" + Guid.NewGuid())
+                .Options;
+
+            _db = new ApplicationDbContext(options);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _db.Dispose();
+        }
+
+
+        private DashboardRepositoryEf CreateRepositoryForCurrentUser(Users currentUser)
+        {
+            var httpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, currentUser.Id)
+                }, "TestAuth"))
+            };
+
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
+
+            var userManager = CreateUserManager(currentUser);
+            Mock.Get(userManager)
+                .Setup(m => m.GetUserAsync(httpContext.User))
+                .ReturnsAsync(currentUser);
+
+            return new DashboardRepositoryEf(_db, userManager, httpContextAccessor.Object);
+        }
+
+        private static UserManager<Users> CreateUserManager(Users currentUser)
+        {
+            var store = new Mock<IUserStore<Users>>();
+            var userManager = new Mock<UserManager<Users>>(
+                store.Object,
+                null!,
+                null!,
+                null!,
+                null!,
+                null!,
+                null!,
+                null!,
+                null!);
+
+            userManager.Setup(m => m.FindByIdAsync(currentUser.Id))
+                .ReturnsAsync(currentUser);
+
+            return userManager.Object;
+        }
+
+        private static Users CreateUser(string userName, string email)
+        {
+            return new Users
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = userName,
+                Email = email,
+                EmailConfirmed = true
+            };
+        }
+
+        private static ReportIssue CreateReport(string userId, string description, string status)
+        {
+            return new ReportIssue
+            {
+                UserId = userId,
+                Description = description,
+                Status = status,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
     }
 }
