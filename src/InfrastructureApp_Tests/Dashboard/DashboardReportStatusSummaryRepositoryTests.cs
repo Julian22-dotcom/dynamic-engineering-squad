ea8705085fc57c413fa575a65c74a7d5bac18f12
Julian Aguilar-Tomas <julianaguilar.tomas17@gmail.com>
2026-05-07 21:22:33 -0700

    Added dashboard status summary test file

---
+++ src/InfrastructureApp_Tests/Dashboard/DashboardReportStatusSummaryRepositoryTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace InfrastructureApp_Tests.Dashboard
+{
+    // SCRUM-142: Tests private Dashboard report status summary counts.
+    [TestFixture]
+    public class DashboardReportStatusSummaryRepositoryTests
+    {
+    }
+}
